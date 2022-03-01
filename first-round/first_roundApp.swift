e404674782cc1e7a4c140952b4eccc9bf99bd47f
Oktavia Citra <oktaviacitraaa@gmail.com>
2022-03-01 18:37:33 +0700

    Initial Commit

---
+++ first-round/first_roundApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_roundApp.swift
+//  first-round
+//
+//  Created by Oktavia Citra on 01/03/22.
+//
+
+import SwiftUI
+
+@main
+struct first_roundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
